725aaae0d0876af3d3510c32dcf27d63550fb410
Shahar Kedar <shahar@bigpanda.io>
2013-02-08 12:14:39 +0200

    Initial import

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'client')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.listen(8000);
+console.log('Express server started on port 8000');
